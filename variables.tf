d1416e3c05fcfb19232d6d5f11ecb66330a44d8d
Shashank-Narayanan-Kyndryl <106687711+Shashank-Narayanan-Kyndryl@users.noreply.github.com>
2022-06-15 15:48:08 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,11 @@
+variable "rgname" {
+  type        = string
+  default     = "rg-USDeclAuto-kcl-eastus-001"
+  description = "Name of our Resource group"
+}
 
+variable "location" {
+  type        = string
+  default     = "eastus"
+  description = "location"
+}
